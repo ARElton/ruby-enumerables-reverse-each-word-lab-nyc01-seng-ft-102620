ffbb824cfb74f80727eeb9d5c7cc357d908fc904
Amelia Elton <ameliareneeelton@gmail.com>
2020-10-08 18:22:22 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,10 +2,10 @@ require "pry"
 
 def reverse_each_word(sentence)
   array = sentence.split 
-  array.collect do |string|
+  array.collect! do |string|
     string.reverse
   end
-  
+  array.join(" ")
 end
 
 binding.pry
