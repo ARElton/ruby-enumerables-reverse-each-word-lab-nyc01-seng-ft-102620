841363f9697cfd1bd0b91dcd7f2af3b99c7489fc
Amelia Elton <ameliareneeelton@gmail.com>
2020-10-08 18:17:01 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,8 +3,8 @@ require "pry"
 def reverse_each_word(sentence)
   array = sentence.split 
   array.collect do |string|
-    string.reverse 
+    string.reverse.join(" ")
   end
-  array.join(" ")
+  
 end
 
