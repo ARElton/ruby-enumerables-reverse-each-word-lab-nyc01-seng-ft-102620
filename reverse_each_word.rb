2f5d31fcc56d5d883172810db7e63189cffb16b3
Amelia Elton <ameliareneeelton@gmail.com>
2020-10-08 18:13:44 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,13 +1,7 @@
 require "pry" 
 
 def reverse_each_word(sentence)
-  array = sentence.split(" ")
-  new_array = []
-  array.each do |string|
-    string.reverse 
-    new_array << string.reverse
-  end
-  new_array.join(" ")
-  
+  array = sentence.split 
+  array.collect 
 end
 
